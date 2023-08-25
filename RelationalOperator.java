fa09e7f84f1c36c19be772d9238ea6bc4c3c48cd
shaibazarkate <shaibazark@gmail.com>
2023-08-26 02:00:46 +0530

    Relational Operator

---
+++ RelationalOperator.java
@@ -0,0 +1,29 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 10 ;
+        int y = 8 ;
+        
+        boolean f = x < y ;
+        System.out.println(f);
+
+         f = x > y ;
+        System.out.println(f);
+
+         f = x >= y ;
+        System.out.println(f);
+
+         f = x != y ;
+        System.out.println(f);
+
+         f = x == y ;
+        System.out.println(f);
+
+        double a = 8.9;
+        double b = 8.7;
+
+        f = a > b ;
+        System.out.println(f);
+
+
+    }
+}
