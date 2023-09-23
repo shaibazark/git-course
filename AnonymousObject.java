534ca6b803119d20078a5170922ee94304e995c9
shaibazarkate <shaibazark@gmail.com>
2023-09-23 19:33:52 +0530

    AnonymousObject

---
+++ AnonymousObject.java
@@ -0,0 +1,15 @@
+class Demo{
+
+public Demo(){
+    System.out.println("In Constr");
+}
+
+}
+
+public class AnonymousObject {
+    public static void main(String[] args){
+
+        //Demo obj = new Demo();
+        new Demo();
+    }
+}
